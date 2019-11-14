6979f99f946a3effffb30036ae4027d162335f12
JoshuaDelavo <50000938+JoshuaDelavo@users.noreply.github.com>
2019-11-14 17:02:32 +0700

    layout awal
    layout utama sama chapter list

--- app/src/main/java/com/example/tubesp3b_3/MainActivity.java
+++ app/src/main/java/com/example/tubesp3b_3/MainActivity.java
@@ -3,9 +3,10 @@ package com.example.tubesp3b_3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
