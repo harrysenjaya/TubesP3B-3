f4faa8517e6a0c9cd6c8a93ff5d875547376cc29
JoshuaDelavo <50000938+JoshuaDelavo@users.noreply.github.com>
2019-11-14 09:28:20 +0700

    Project

---
+++ app/src/main/java/com/example/tubesp3b_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.tubesp3b_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
