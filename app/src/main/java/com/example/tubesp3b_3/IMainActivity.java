ad12f2ec79a753fe36ff11ccbdb0cd5100f788b7
Harry Senjaya <harxsenjaya56@gmail.com>
2019-11-18 14:34:05 +0700

    update

--- app/src/main/java/com/example/tubesp3b_3/IMainActivity.java
+++ app/src/main/java/com/example/tubesp3b_3/IMainActivity.java
@@ -3,6 +3,10 @@ package com.example.tubesp3b_3;
 import java.util.ArrayList;
 
 public interface IMainActivity {
+<<<<<<< HEAD
     void getMangaList(ArrayList<Manga> manga);
 
+=======
+    void
+>>>>>>> ca63ed08100618da1816eb0203b34b7fc52dd416
 }
