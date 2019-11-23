b20bbe300e4e9f9495476e8b10d8e09adea74ae8
ChristopherLunadi <55026909+exaponz@users.noreply.github.com>
2019-11-23 23:00:50 +0700

    Revert "g jelas :'>"
    This reverts commit 42a9af8501c78c2714ef243d9285e52a89068c27.

--- app/src/main/java/com/example/tubesp3b_3/MainFragment.java
+++ app/src/main/java/com/example/tubesp3b_3/MainFragment.java
@@ -5,21 +5,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.GridView;
-import android.widget.ListView;
-import android.widget.SearchView;
-import android.widget.Spinner;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,18 +20,6 @@ public class MainFragment extends Fragment  {
 
     @BindView(R.id.gridView)
     GridView grid;
-
-    @BindView(R.id.spinnerSort)
-    Spinner spinnerSort;
-
-    @BindView(R.id.search)
-    SearchView search;
-
-    @BindView(R.id.btnSort)
-    Button btn_Sort;
-
-    ArrayList<Manga> nAdapter;
-    ArrayList<Manga> mAdapter;
 
     private FragmentManager fragmentManager;
     private FragmentTransaction ft;
@@ -80,81 +60,9 @@ public class MainFragment extends Fragment  {
     }
 
     public void Create(ArrayList<Manga> manga){
-      /*
-        nAdapter.sort(new Comparator<Manga>() {
-            @Override
-            public int compare(Manga manga, Manga t1) {
-                return manga.getTitle().compareTo(t1.getTitle());
-            }
-        });
-
-        mAdapter.sort(new Comparator<Manga>() {
-                          @Override
-                          public int compare(Manga manga, Manga t1) {
-                              return t1.getTitle().compareTo(manga.getTitle());
-                          }
-        });
-*/
-
         GridAdapter adapter = new GridAdapter(this.getActivity(), manga);
         grid.setAdapter(adapter);
-
-        String[] options = new String[] {"Hits", "A - Z", "Z - A"};
-        ArrayAdapter<String> bAdapter = new ArrayAdapter<String>(this.getContext() , android.R.layout.simple_spinner_dropdown_item, options);
-        this.spinnerSort.setAdapter(bAdapter);
-       /* this.btn_Sort.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String spinnerT = spinnerSort.getSelectedItem().toString();
-
-                if(spinnerT.equals("Hits")){
-                    GridAdapter adapter = new GridAdapter(getActivity(), manga);
-                    grid.setAdapter(adapter);
-                }
-                else if(spinnerT.equals("A - Z")){
-                    newGrid1();
-                    GridAdapter adapter = new GridAdapter(getActivity(), nAdapter);
-                    grid.setAdapter(adapter);
-                }
-               else{
-                    newGrid2(manga);
-                    GridAdapter adapter = new GridAdapter(getActivity(), mAdapter);
-                    grid.setAdapter(adapter);
-                }
-
-            }
-        });*/
     }
-
-   /* public void newGrid1() {
-        Comparator<Manga> mangaName = new Comparator<Manga>() {
-            @Override
-            public int compare(Manga manga, Manga t1) {
-                String m1 = manga.getTitle().toUpperCase();
-                String m2 = t1.getTitle().toUpperCase();
-
-                return m1.compareTo(m2);
-            }
-        };
-    }
-        /*nAdapter.sort(new Comparator<Manga>() {
-            @Override
-            public int compare(Manga manga, Manga t1) {
-                return manga.getTitle().compareTo(t1.getTitle());
-            }
-        });*/
-
-
-    public void newGrid2(ArrayList<Manga> manga){
-        mAdapter.sort(new Comparator<Manga>() {
-            @Override
-            public int compare(Manga manga, Manga t1) {
-                return t1.getTitle().compareTo(manga.getTitle());
-            }
-        });
-    }
-
-
 
     public void MangaInfo(int id, int position){
         this.presenter.getMangaInfo(position);
